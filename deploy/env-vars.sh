5b5ed8f728b26910f03203bd4792743dfb21f050
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-08-18 23:13:05 +0100

    Update env-vars.sh

--- deploy/env-vars.sh
+++ deploy/env-vars.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-set -euo
+set -euo pipefail
 
-sed -i "s, RPC_ADDRESS, '${hasura-graphql-engine.PRIVATE_URL}',g" .bdjuno/config.yaml
+sed -i "s, RPC_ADDRESS, '${RPC_ADDRESS}',g" .bdjuno/config.yaml
+sed -i "s, GRPC_ADDRESS, '${GRPC_ADDRESS}',g" .bdjuno/config.yaml
+sed -i "s, LOG_LEVEL, '${LOG_LEVEL}',g" .bdjuno/config.yaml
+sed -i "s, DATABASE_NAME, '${DATABASE}',g" .bdjuno/config.yaml
+sed -i "s, DATABASE_HOST, '${HOSTNAME}',g" .bdjuno/config.yaml
+sed -i "s, DATABASE_PORT, '${PORT}',g" .bdjuno/config.yaml
+sed -i "s, DATABASE_USER, '${USERNAME}',g" .bdjuno/config.yaml
+sed -i "s, DATABASE_PASSWORD, '${PASSWORD}',g" .bdjuno/config.yaml
+sed -i "s, ACTIONS_BASE_URL, '${ACTIONS_BASE_URL}',g" .bdjuno/config.yaml
