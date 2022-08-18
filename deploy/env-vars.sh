4669cdce3d77daa95bb36d0262d15a56bb28b917
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-08-19 00:58:56 +0100

    fix: Variable path

--- deploy/env-vars.sh
+++ deploy/env-vars.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-sed -i "s, RPC_ADDRESS, '${RPC_ADDRESS}',g" /bdjuno/.bdjuno/config.yaml
-sed -i "s, GRPC_ADDRESS, '${GRPC_ADDRESS}',g" /bdjuno/.bdjuno/config.yaml
-sed -i "s, LOG_LEVEL, '${LOG_LEVEL}',g" /bdjuno/.bdjuno/config.yaml
-sed -i "s, DATABASE_NAME, '${testnet-explorer-database.DATABASE}',g" /bdjuno/.bdjuno/config.yaml
-sed -i "s, DATABASE_HOST, '${testnet-explorer-database.HOSTNAME}',g" /bdjuno/.bdjuno/config.yaml
-sed -i "s, DATABASE_PORT, '${testnet-explorer-database.PORT}',g" /bdjuno/.bdjuno/config.yaml
-sed -i "s, DATABASE_USER, '${testnet-explorer-database.USERNAME}',g" /bdjuno/.bdjuno/config.yaml
-sed -i "s, DATABASE_PASSWORD, '${testnet-explorer-database.PASSWORD}',g" /bdjuno/.bdjuno/config.yaml
+sed -i "s, RPC_ADDRESS, '${RPC_ADDRESS}',g" ./.bdjuno/config.yaml
+sed -i "s, GRPC_ADDRESS, '${GRPC_ADDRESS}',g" ./.bdjuno/config.yaml
+sed -i "s, LOG_LEVEL, '${LOG_LEVEL}',g" ./.bdjuno/config.yaml
+sed -i "s, DATABASE_NAME, '${testnet-explorer-database.DATABASE}',g" ./.bdjuno/config.yaml
+sed -i "s, DATABASE_HOST, '${testnet-explorer-database.HOSTNAME}',g" ./.bdjuno/config.yaml
+sed -i "s, DATABASE_PORT, '${testnet-explorer-database.PORT}',g" ./.bdjuno/config.yaml
+sed -i "s, DATABASE_USER, '${testnet-explorer-database.USERNAME}',g" ./.bdjuno/config.yaml
+sed -i "s, DATABASE_PASSWORD, '${testnet-explorer-database.PASSWORD}',g" ./.bdjuno/config.yaml
