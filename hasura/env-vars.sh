d1cf11b378688f620c1cf7359e33160d40f9c0f5
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-08-18 23:57:05 +0100

    Update env-vars.sh

--- hasura/env-vars.sh
+++ hasura/env-vars.sh
@@ -2,11 +2,5 @@
 
 set -euo pipefail
 
-sed -i "s, RPC_ADDRESS, '${RPC_ADDRESS}',g" .bdjuno/config.yaml
-sed -i "s, GRPC_ADDRESS, '${GRPC_ADDRESS}',g" .bdjuno/config.yaml
-sed -i "s, LOG_LEVEL, '${LOG_LEVEL}',g" .bdjuno/config.yaml
-sed -i "s, DATABASE_NAME, '${testnet-explorer-database.DATABASE}',g" .bdjuno/config.yaml
-sed -i "s, DATABASE_HOST, '${testnet-explorer-database.HOSTNAME}',g" .bdjuno/config.yaml
-sed -i "s, DATABASE_PORT, '${testnet-explorer-database.PORT}',g" .bdjuno/config.yaml
-sed -i "s, DATABASE_USER, '${testnet-explorer-database.USERNAME}',g" .bdjuno/config.yaml
-sed -i "s, DATABASE_PASSWORD, '${testnet-explorer-database.PASSWORD}',g" .bdjuno/config.yaml
+sed -i "s, HASURA_GRAPHQL_ENDPOINT, '${HASURA_GRAPHQL_ENDPOINT}',g" config.yaml
+sed -i "s, ACTIONS_BASE_URL, '${ACTIONS_BASE_URL}',g" config.yaml
