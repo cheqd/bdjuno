2b13599bb14e411f8075629e91be6b310e8f9628
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-08-18 23:49:44 +0100

    Squashed commit of the following:
    commit 927e888a946087a09df041778f9bbd0df47c50cb
    Author: Ankur Banerjee <ankurdotb@users.noreply.github.com>
    Date:   Thu Aug 18 23:48:34 2022 +0100
    
        Update env-vars.sh

--- deploy/env-vars.sh
+++ deploy/env-vars.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 sed -i "s, RPC_ADDRESS, '${RPC_ADDRESS}',g" .bdjuno/config.yaml
 sed -i "s, GRPC_ADDRESS, '${GRPC_ADDRESS}',g" .bdjuno/config.yaml
 sed -i "s, LOG_LEVEL, '${LOG_LEVEL}',g" .bdjuno/config.yaml
-sed -i "s, DATABASE_NAME, '${DATABASE}',g" .bdjuno/config.yaml
-sed -i "s, DATABASE_HOST, '${HOSTNAME}',g" .bdjuno/config.yaml
-sed -i "s, DATABASE_PORT, '${PORT}',g" .bdjuno/config.yaml
-sed -i "s, DATABASE_USER, '${USERNAME}',g" .bdjuno/config.yaml
-sed -i "s, DATABASE_PASSWORD, '${PASSWORD}',g" .bdjuno/config.yaml
+sed -i "s, DATABASE_NAME, '${testnet-explorer-database.DATABASE}',g" .bdjuno/config.yaml
+sed -i "s, DATABASE_HOST, '${testnet-explorer-database.HOSTNAME}',g" .bdjuno/config.yaml
+sed -i "s, DATABASE_PORT, '${testnet-explorer-database.PORT}',g" .bdjuno/config.yaml
+sed -i "s, DATABASE_USER, '${testnet-explorer-database.USERNAME}',g" .bdjuno/config.yaml
+sed -i "s, DATABASE_PASSWORD, '${testnet-explorer-database.PASSWORD}',g" .bdjuno/config.yaml
