2bfd52974fe89aa35f9ac9f7e5889bdac8de9052
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-08-19 01:36:23 +0100

    fix: Vars again

--- deploy/env-vars.sh
+++ deploy/env-vars.sh
@@ -3,9 +3,9 @@
 
 set -euo pipefail
 
-sed -i "s, RPC_ADDRESS, '${bdjuno.RPC_ADDRESS}',g" ./.bdjuno/config.yaml
-sed -i "s, GRPC_ADDRESS, '${bdjuno.GRPC_ADDRESS}',g" ./.bdjuno/config.yaml
-sed -i "s, LOG_LEVEL, '${bdjuno.LOG_LEVEL}',g" ./.bdjuno/config.yaml
+sed -i "s, RPC_ADDRESS, '${_self.RPC_ADDRESS}',g" ./.bdjuno/config.yaml
+sed -i "s, GRPC_ADDRESS, '${_self.GRPC_ADDRESS}',g" ./.bdjuno/config.yaml
+sed -i "s, LOG_LEVEL, '${_self.LOG_LEVEL}',g" ./.bdjuno/config.yaml
 sed -i "s, DATABASE_NAME, '${testnet-explorer-database.DATABASE}',g" ./.bdjuno/config.yaml
 sed -i "s, DATABASE_HOST, '${testnet-explorer-database.HOSTNAME}',g" ./.bdjuno/config.yaml
 sed -i "s, DATABASE_PORT, '${testnet-explorer-database.PORT}',g" ./.bdjuno/config.yaml
