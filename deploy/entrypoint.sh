a29f6cc55bbc94e49775b63c344a5d9dfaf03e8f
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-08-19 02:50:31 +0100

    Update entrypoint.sh

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 # Change configuration file values
-echo ${RPC_ADDRESS}
-echo ${testnet-explorer-database.DATABASE}
+echo "${RPC_ADDRESS}"
+echo "${testnet-explorer-database.DATABASE}"
 
 sed -i "s, DATABASE_NAME, '${testnet-explorer-database.DATABASE}',g" /bdjuno/.bdjuno/config.yaml
 sed -i "s, DATABASE_HOST, '${testnet-explorer-database.HOSTNAME}',g" /bdjuno/.bdjuno/config.yaml
