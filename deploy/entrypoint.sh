eedd69afc11c4d8b532a854b50d499fca81f1017
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-08-19 03:39:21 +0100

    All entrypoint values fixed

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -2,12 +2,14 @@
 
 set -euo pipefail
 
-# Change configuration file values
-echo "${RPC_ADDRESS}"
-echo "${DATABASE_NAME}"
-
+sed -i "s, RPC_ADDRESS, '${RPC_ADDRESS}',g" /bdjuno/.bdjuno/config.yaml
+sed -i "s, MAX_RPC_CONNECTIONS, '${MAX_RPC_CONNECTIONS}',g" /bdjuno/.bdjuno/config.yaml
+sed -i "s, GRPC_ADDRESS, '${GRPC_ADDRESS}',g" /bdjuno/.bdjuno/config.yaml
+sed -i "s, LOG_LEVEL, '${LOG_LEVEL}',g" /bdjuno/.bdjuno/config.yaml
 sed -i "s, DATABASE_NAME, '${DATABASE_NAME}',g" /bdjuno/.bdjuno/config.yaml
 sed -i "s, DATABASE_HOST, '${DATABASE_HOST}',g" /bdjuno/.bdjuno/config.yaml
 sed -i "s, DATABASE_PORT, '${DATABASE_PORT}',g" /bdjuno/.bdjuno/config.yaml
 sed -i "s, DATABASE_USER, '${DATABASE_USER}',g" /bdjuno/.bdjuno/config.yaml
 sed -i "s, DATABASE_PASSWORD, '${DATABASE_PASSWORD}',g" /bdjuno/.bdjuno/config.yaml
+
+bdjuno start --home /bdjuno/.bdjuno
