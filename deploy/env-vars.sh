d77c1a166f4520c2851fa9f5ca5aba6f7c1dda0d
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-08-19 01:50:14 +0100

    refactor: Set some variables directly

--- deploy/env-vars.sh
+++ deploy/env-vars.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# shellcheck disable=SC2154
 
 set -euo pipefail
 
-sed -i "s, RPC_ADDRESS, '${_self.RPC_ADDRESS}',g" ./.bdjuno/config.yaml
-sed -i "s, GRPC_ADDRESS, '${_self.GRPC_ADDRESS}',g" ./.bdjuno/config.yaml
-sed -i "s, LOG_LEVEL, '${_self.LOG_LEVEL}',g" ./.bdjuno/config.yaml
-sed -i "s, DATABASE_NAME, '${testnet-explorer-database.DATABASE}',g" ./.bdjuno/config.yaml
-sed -i "s, DATABASE_HOST, '${testnet-explorer-database.HOSTNAME}',g" ./.bdjuno/config.yaml
-sed -i "s, DATABASE_PORT, '${testnet-explorer-database.PORT}',g" ./.bdjuno/config.yaml
-sed -i "s, DATABASE_USER, '${testnet-explorer-database.USERNAME}',g" ./.bdjuno/config.yaml
-sed -i "s, DATABASE_PASSWORD, '${testnet-explorer-database.PASSWORD}',g" ./.bdjuno/config.yaml
+sed -i "s, DATABASE_NAME, '${testnet-explorer-database.DATABASE}',g" ~/.bdjuno/config.yaml
+sed -i "s, DATABASE_HOST, '${testnet-explorer-database.HOSTNAME}',g" ~/.bdjuno/config.yaml
+sed -i "s, DATABASE_PORT, '${testnet-explorer-database.PORT}',g" ~/.bdjuno/config.yaml
+sed -i "s, DATABASE_USER, '${testnet-explorer-database.USERNAME}',g" ~/.bdjuno/config.yaml
+sed -i "s, DATABASE_PASSWORD, '${testnet-explorer-database.PASSWORD}',g" ~/.bdjuno/config.yaml
