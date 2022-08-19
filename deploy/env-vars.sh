c4bf936957c2497f42dc964c8632cf6b55d57ae5
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-08-19 01:18:26 +0100

    fix: Explicitly set variable namespace

--- deploy/env-vars.sh
+++ deploy/env-vars.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-sed -i "s, RPC_ADDRESS, '${RPC_ADDRESS}',g" ./.bdjuno/config.yaml
-sed -i "s, GRPC_ADDRESS, '${GRPC_ADDRESS}',g" ./.bdjuno/config.yaml
-sed -i "s, LOG_LEVEL, '${LOG_LEVEL}',g" ./.bdjuno/config.yaml
+sed -i "s, RPC_ADDRESS, '${bdjuno.RPC_ADDRESS}',g" ./.bdjuno/config.yaml
+sed -i "s, GRPC_ADDRESS, '${bdjuno.GRPC_ADDRESS}',g" ./.bdjuno/config.yaml
+sed -i "s, LOG_LEVEL, '${bdjuno.LOG_LEVEL}',g" ./.bdjuno/config.yaml
 sed -i "s, DATABASE_NAME, '${testnet-explorer-database.DATABASE}',g" ./.bdjuno/config.yaml
 sed -i "s, DATABASE_HOST, '${testnet-explorer-database.HOSTNAME}',g" ./.bdjuno/config.yaml
 sed -i "s, DATABASE_PORT, '${testnet-explorer-database.PORT}',g" ./.bdjuno/config.yaml
