114a0ce45b6f485c6278ff7f26b5e72c2e54aa0f
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-08-19 03:51:12 +0100

    Update entrypoint.sh

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -7,7 +7,6 @@ sed -i "s, GRPC_ADDRESS, '${GRPC_ADDRESS}',g" /bdjuno/.bdjuno/config.yaml
 sed -i "s, LOG_LEVEL, '${LOG_LEVEL}',g" /bdjuno/.bdjuno/config.yaml
 sed -i "s, DATABASE_NAME, '${DATABASE_NAME}',g" /bdjuno/.bdjuno/config.yaml
 sed -i "s, DATABASE_HOST, '${DATABASE_HOST}',g" /bdjuno/.bdjuno/config.yaml
-sed -i "s, DATABASE_PORT, '${DATABASE_PORT}',g" /bdjuno/.bdjuno/config.yaml
 sed -i "s, DATABASE_USER, '${DATABASE_USER}',g" /bdjuno/.bdjuno/config.yaml
 sed -i "s, DATABASE_PASSWORD, '${DATABASE_PASSWORD}',g" /bdjuno/.bdjuno/config.yaml
 
