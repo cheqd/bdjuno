0ef3a6b4336c18d17c3cb6a8fe9201ac3fd52ef5
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-08-19 02:18:15 +0100

    Update env-vars.sh

--- deploy/env-vars.sh
+++ deploy/env-vars.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-sed -i "s, DATABASE_NAME, '${testnet-explorer-database.DATABASE}',g" config.yaml
-sed -i "s, DATABASE_HOST, '${testnet-explorer-database.HOSTNAME}',g" config.yaml
-sed -i "s, DATABASE_PORT, '${testnet-explorer-database.PORT}',g" config.yaml
-sed -i "s, DATABASE_USER, '${testnet-explorer-database.USERNAME}',g" config.yaml
-sed -i "s, DATABASE_PASSWORD, '${testnet-explorer-database.PASSWORD}',g" config.yaml
+sed -i "s, DATABASE_NAME, '${testnet-explorer-database.DATABASE}',g" /bdjuno/.bdjuno/config.yaml
+sed -i "s, DATABASE_HOST, '${testnet-explorer-database.HOSTNAME}',g" /bdjuno/.bdjuno/config.yaml
+sed -i "s, DATABASE_PORT, '${testnet-explorer-database.PORT}',g" /bdjuno/.bdjuno/config.yaml
+sed -i "s, DATABASE_USER, '${testnet-explorer-database.USERNAME}',g" /bdjuno/.bdjuno/config.yaml
+sed -i "s, DATABASE_PASSWORD, '${testnet-explorer-database.PASSWORD}',g" /bdjuno/.bdjuno/config.yaml
