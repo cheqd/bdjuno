9b4fbf5ddea22fe50dd444179989bac9c9e5fb1b
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-08-23 16:57:33 +0100

    Update entrypoint.sh

--- hasura/entrypoint.sh
+++ hasura/entrypoint.sh
@@ -4,3 +4,5 @@ set -euo pipefail
 
 sed -i "s, HASURA_GRAPHQL_ENDPOINT, '${HASURA_GRAPHQL_ENDPOINT}',g" config.yaml
 sed -i "s, ACTIONS_BASE_URL, '${ACTIONS_BASE_URL}',g" config.yaml
+
+hasura metadata apply --config /hasura/config.yaml
