02266a17cf71a9f378136feea891d9b926ae128f
Filip Djokic <djokicf@protonmail.com>
2022-09-08 13:48:40 +0200

    fix: Redeploy bdjuno on DO Apps
    Signed-off-by: Filip Djokic <djokicf@protonmail.com>

--- hasura/entrypoint.sh
+++ hasura/entrypoint.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 sed -i "s, HASURA_GRAPHQL_ENDPOINT, '${HASURA_GRAPHQL_ENDPOINT}',g" config.yaml
-sed -i "s, ACTIONS_BASE_URL, '${ACTIONS_BASE_URL}',g" config.yaml
+sed -i "s, ACTION_BASE_URL, '${ACTION_BASE_URL}',g" config.yaml
 
-hasura metadata apply --config /hasura/config.yaml
+hasura metadata apply --endpoint $HASURA_GRAPHQL_ENDPOINT --admin-secret $HASURA_GRAPHQL_ADMIN_SECRET
