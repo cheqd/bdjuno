b2fd27c24e935284296efd919a8f08d390624f70
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-09-08 13:06:48 +0100

    fix SC2086 violation

--- hasura/entrypoint.sh
+++ hasura/entrypoint.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 sed -i "s, HASURA_GRAPHQL_ENDPOINT, '${HASURA_GRAPHQL_ENDPOINT}',g" config.yaml
 sed -i "s, ACTION_BASE_URL, '${ACTION_BASE_URL}',g" config.yaml
 
-hasura metadata apply --endpoint $HASURA_GRAPHQL_ENDPOINT --admin-secret $HASURA_GRAPHQL_ADMIN_SECRET
+hasura metadata apply --endpoint "$HASURA_GRAPHQL_ENDPOINT" --admin-secret "$HASURA_GRAPHQL_ADMIN_SECRET"
