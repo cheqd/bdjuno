b3a1236f4c216d30e516b445d1352f76b0a0d977
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2023-02-17 17:20:30 +0000

    Update proposal.go

--- cmd/parse/gov/proposal.go
+++ cmd/parse/gov/proposal.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	modulestypes "github.com/forbole/bdjuno/v3/modules/types"
-	"github.com/forbole/bdjuno/v3/utils"
 	"github.com/rs/zerolog/log"
 
 	govtypesv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
