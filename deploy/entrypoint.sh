0b6b7a2b04e5a3054a3717bc26c8ed9dd916cb4b
Filip Djokic <djokicf@protonmail.com>
2024-04-09 15:32:31 +0200

    Update entrypotint.sh

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 # Inject environment variables into config.yaml file
 
-sed -i "s, RPC_ADDRESS, '${RPC_ADDRESS}',g" /bdjuno/.bdjuno/config.yaml
-sed -i "s, GRPC_ADDRESS, '${GRPC_ADDRESS}',g" /bdjuno/.bdjuno/config.yaml
-sed -i "s, LOG_LEVEL, '${LOG_LEVEL}',g" /bdjuno/.bdjuno/config.yaml
-sed -i "s, DATABASE_URL, '${DATABASE_URL}',g" /bdjuno/.bdjuno/config.yaml
+sed -i "s, RPC_ADDRESS, '${RPC_ADDRESS}',g" /callisto/.callisto/config.yaml
+sed -i "s, GRPC_ADDRESS, '${GRPC_ADDRESS}',g" /callisto/.callisto/config.yaml
+sed -i "s, LOG_LEVEL, '${LOG_LEVEL}',g" /callisto/.callisto/config.yaml
+sed -i "s, DATABASE_URL, '${DATABASE_URL}',g" /callisto/.callisto/config.yaml
 
-bdjuno start --home /bdjuno/.bdjuno
+callisto start --home /callisto/.callisto
